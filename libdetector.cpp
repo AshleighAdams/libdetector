5cbec6ab6decf98f828b9e295acd723f8a17fcef
c0bra61 <c0bra@xiatek.org>
2011-07-27 00:46:42 +0100

    Winderps

--- libdetector.cpp
+++ libdetector.cpp
@@ -16,12 +16,83 @@ using namespace Detector;
 #define PIXEL_MOTION 1
 #define PIXEL_SCANNEDMOTION 2
 
+#ifdef WINDOWS // We need a standard for this...
+/*
+    Thanks to Carl Staelin for this snippet
+    http://stackoverflow.com/questions/5404277/porting-clock-gettime-to-windows
+*/
+#define CLOCKS_PER_SEC 0
+LARGE_INTEGER getFILETIMEoffset()
+{
+	SYSTEMTIME s;
+	FILETIME f;
+	LARGE_INTEGER t;
+
+	s.wYear = 1970;
+	s.wMonth = 1;
+	s.wDay = 1;
+	s.wHour = 0;
+	s.wMinute = 0;
+	s.wSecond = 0;
+	s.wMilliseconds = 0;
+	SystemTimeToFileTime( &s, &f );
+	t.QuadPart = f.dwHighDateTime;
+	t.QuadPart <<= 32;
+	t.QuadPart |= f.dwLowDateTime;
+	return t;
+}
+
+int clock_gettime( int X, struct timeval* tv )
+{
+	LARGE_INTEGER           t;
+	FILETIME                f;
+	double                  microseconds;
+	static LARGE_INTEGER    offset;
+	static double           frequencyToMicroseconds;
+	static int              initialized = 0;
+	static BOOL             usePerformanceCounter = 0;
+
+	if ( !initialized )
+	{
+		LARGE_INTEGER performanceFrequency;
+		initialized = 1;
+		usePerformanceCounter = QueryPerformanceFrequency( &performanceFrequency );
+		if ( usePerformanceCounter )
+		{
+			QueryPerformanceCounter( &offset );
+			frequencyToMicroseconds = ( double )performanceFrequency.QuadPart / 1000000.;
+		}
+		else
+		{
+			offset = getFILETIMEoffset();
+			frequencyToMicroseconds = 10.;
+		}
+	}
+	if ( usePerformanceCounter )
+        QueryPerformanceCounter( &t );
+	else
+	{
+		GetSystemTimeAsFileTime( &f );
+		t.QuadPart = f.dwHighDateTime;
+		t.QuadPart <<= 32;
+		t.QuadPart |= f.dwLowDateTime;
+	}
+
+	t.QuadPart -= offset.QuadPart;
+	microseconds = ( double )t.QuadPart / frequencyToMicroseconds;
+	t.QuadPart = microseconds;
+	tv->tv_sec = t.QuadPart / 1000000;
+	tv->tv_usec = t.QuadPart % 1000000;
+	return 0;
+}
+#endif // WINDOWS
+
 double Detector::GetCurrentTime()
 {
-    struct timespec now;
-    clock_gettime(CLOCK_MONOTONIC, &now);
+	struct timespec now;
+	clock_gettime( CLOCK_MONOTONIC, &now );
 
-    return (double)(now.tv_nsec / CLOCKS_PER_SEC) / 1000.0 + (double)now.tv_sec;
+	return ( double )( now.tv_nsec / CLOCKS_PER_SEC ) / 1000.0 + ( double )now.tv_sec;
 }
 
 bool Detector::imagesize_tEqual( imagesize_t a, imagesize_t b )
@@ -211,12 +282,12 @@ bool CDetector::PushImage( CDetectorImage* pImage )
 			}
 
 			m_pTargets[count++] = targ;
-			if(count == MAX_TARGETS) // Max targets have been reached, just escape the loop
-                goto EndLoop;
+			if( count == MAX_TARGETS ) // Max targets have been reached, just escape the loop
+				goto EndLoop;
 		}
 	}
 
-	EndLoop:
+EndLoop:
 
 	m_iTargets = count;
 	m_pLastImage->UnRefrence();
@@ -256,10 +327,10 @@ void CDetector::SetMinTargSize( float flAmmount )
 
 /// Tracked object stuff
 
-CTrackedObject::CTrackedObject(imagesize_t ImgSize, targetid ID)
+CTrackedObject::CTrackedObject( imagesize_t ImgSize, targetid ID )
 {
-    m_ImageSize = ImgSize;
-    m_tiID = ID;
+	m_ImageSize = ImgSize;
+	m_tiID = ID;
 }
 
 CTrackedObject::~CTrackedObject()
@@ -267,7 +338,7 @@ CTrackedObject::~CTrackedObject()
 
 double CTrackedObject::LastSeen()
 {
-    return m_dblLastSeen;
+	return m_dblLastSeen;
 }
 
 
